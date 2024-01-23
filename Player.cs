7b938e7de3464670f2e6063459cc8ffd145bd380
DumBMiS <dominikociepka4@gmail.com>
2024-01-23 19:12:13 +0100

    add properties to Player class

---
+++ Player.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using Rectangle = System.Windows.Shapes.Rectangle;
+namespace WpfApp7
+{
+    public class Player : ICanvasObject
+    {
+        public double X { get; set; }
+        public double Y { get; set; }
+        public int Width { get; set; }
+        public int Height { get; set; }
+        public Canvas Canvas { get; set; }
+        public Rectangle Shape { get; set; }
+        public bool MousePlayer { get; set; }
+        public int Points { get; set; }
+        public void Draw()
+        {
+
+        }
+        public void Reset()
+        {
+
+        }
+    }
+}
